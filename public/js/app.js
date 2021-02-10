849246648724eb18ce204d075513b0a049b088c6
David Pinheiro <dspinheiro@ualg.pt>
2021-02-10 15:14:32 +0000

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("JS file loaded");
-
 const weatherForm = document.querySelector("form");
 const searchElement = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
